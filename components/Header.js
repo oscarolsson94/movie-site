d321df9b809b82a36d966f61c18bb23f026a216e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 10:40:00 +0200

    styling header

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon
 
 const Header = () => {
     return (
-        <header className="" >
-            <div className="flex" >
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto" >
+            <div className="flex flex-grow justify-evenly max-w-2xl" >
                 <HeaderItem title="Home" Icon={HomeIcon} />
                 <HeaderItem title="Trending" Icon={LightningBoltIcon} />
                 <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
