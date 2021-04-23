764002bec938b384b112550b23c4a31aa29113c4
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 12:36:54 +0200

    working on SSR

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
 
 export default function Home() {
   return (
@@ -12,11 +13,16 @@ export default function Home() {
           
         <Header />
           
-        {/* Navbar */}
         <Nav />
 
-        {/* Results */}
+        <Results />
 
     </div>
   )
+}
+
+
+//server side rendering
+export async function getServerSideProps(context) {
+    const genre = context.query.genre;
 }
