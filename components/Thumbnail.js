fc7f27766352c75f40d7d15fd9d8aa00b4efefbc
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 14:08:07 +0200

    adding description and title to each movie

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -12,7 +12,11 @@ const Thumbnail = ({ result }) => {
                 height={1080}
                 width={1920}
             />
-            <div>
+            <div className="p-2" >
+                <p className="truncate max-w-md" >{result.overview}</p>
+                <h2>
+                    {result.title || result.original_name}
+                </h2>
                 <p></p>
             </div>
 
