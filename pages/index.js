cf2a1b079787b3e5bb13f0782c5dcce8407ce904
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 10:57:00 +0200

    setting up request urls and starting nav

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import Nav from '../components/Nav';
 
 export default function Home() {
   return (
@@ -9,12 +10,12 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
           
-          {/* Header */}
-          <Header />
+        <Header />
           
-          {/* Navbar */}
+        {/* Navbar */}
+        <Nav />
 
-          {/* Results */}
+        {/* Results */}
 
     </div>
   )
