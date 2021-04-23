2daf3a503eb6618ba937ae1a1e04e201b26c96d9
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 13:07:31 +0200

    putting the images on the screen

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,7 +1,21 @@
-const Thumbnail = ({result}) => {
+import Image from "next/image";
+
+const Thumbnail = ({ result }) => {
+    
+    const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
     return (
         <div>
-            <h1>Tumbnail</h1>
+            <Image
+                layout="responsive"
+                src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
+                height={1080}
+                width={1920}
+            />
+            <div>
+                <p></p>
+            </div>
+
         </div>
     );
 }
