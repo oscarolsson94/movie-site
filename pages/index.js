52f7e608774dc0fed6d860623115d2cfb1abd916
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 12:46:27 +0200

    setting up SSR and grabbing json response in props

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,37 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Results from '../components/Results';
+import requests from '../utils/requests';
 
 export default function Home() {
-  return (
-    <div>
-        <Head>
-            <title>Moviesite</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+    return (
+        <div>
+            <Head>
+                <title>Moviesite</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
           
-        <Header />
+            <Header />
           
-        <Nav />
+            <Nav />
 
-        <Results />
+            <Results />
 
-    </div>
-  )
+        </div>
+    );
 }
 
 
 //server side rendering
 export async function getServerSideProps(context) {
     const genre = context.query.genre;
+
+    const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`)
+        .then(res => res.json());
+    
+    return {
+        props: {
+            results: request.results,
+        }
+    };
 }
