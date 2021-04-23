a24f7a232cd304b28fc7c97ee43ee11522254dce
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-23 14:26:00 +0200

    adding some user experience tweaks

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -6,7 +6,7 @@ const Thumbnail = ({ result }) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
     return (
-        <div>
+        <div className="group cursor-pointer" >
             <Image
                 layout="responsive"
                 src={`${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`}
@@ -15,10 +15,12 @@ const Thumbnail = ({ result }) => {
             />
             <div className="p-2" >
                 <p className="truncate max-w-md" >{result.overview}</p>
-                <h2>
+
+                <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold" >
                     {result.title || result.original_name}
                 </h2>
-                <p>
+
+                <p className="flex items-center opacity-0 group-hover:opacity-100" >
                     {result.media_type && `${result.media_type} • `}
                     {result.release_date || result.first_air_date}{" • "}
                     <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
